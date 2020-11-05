7be2bb3d3c1df89db8ef9482aee41f5b28f2df5e
michaelDonchenko <michael1994d@gmail.com>
2020-11-05 13:23:46 +0200

    change projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,50 +2,46 @@ import React from 'react'
 import ProjectItem from './ProjectItem'
 
 const Projects = () => {
-
-const projects = [
-    { name: "Ecommerce-App",
-    description: "This is a fully working ecommerce app, There are two types of users regular users and admin. Regular users can buy products and see their purchase history on thier dashboard, while admins can add/delete products && categories, And see all the orders from the users.",
-    size: "Medium-large",
-    builtWith:"MERN Stack (Context-API)",
-    img: "Ecommerce.PNG",
-    githubLink:"https://github.com/michaelDonchenko/Ecommerce-App",
-    demoLink:"https://ecommerce-app12.herokuapp.com/"
+  const projects = [
+    {
+      name: 'Pro-Shop',
+      description:
+        'My recent project Full ecommerce app. A lot of admin user functionallity like CRUD products view and update users and view and update orders. Users can buy products with paypal or credit card and see thier order history, Users can rate and comment the products, fully working authentication for logged in users and for admins.',
+      size: 'Medium-large',
+      builtWith: 'MERN Stack (Redux)',
+      img: 'proShop.PNG',
+      githubLink: 'https://github.com/michaelDonchenko/proShop',
+      demoLink: 'https://pro-shop01.herokuapp.com/',
     },
 
-    { name: "IT-Logger",
-    description: "IT-Logger is an app for IT department, here the technichians can add/remove/update thier IT logs. Another feature is adding and delling technichians, And making the important in case it needs extra attention.",
-    size: "Small-Medium",
-    builtWith:"MERN Stack (Redux)",
-    img: "itLogger.PNG",
-    githubLink:"https://github.com/michaelDonchenko/IT-looger",
-    demoLink:"https://it-looger.herokuapp.com/"
+    {
+      name: 'MERN-Auth-Template',
+      description:
+        'This is a Complete MERN authentication app I have made from with Context-API. Fully working authentication with login/register errors, anti hacking username && email, JWT and more. ',
+      size: 'Small',
+      builtWith: 'MERN Stack (Context-API)',
+      img: 'mernAuth.PNG',
+      githubLink: 'https://github.com/michaelDonchenko/mern-auth-V2',
+      demoLink: 'https://mern-auth-v2.herokuapp.com/',
     },
 
-    { name: "Github-Finder",
-      description: "Github-Finder is a small app where you can search for github users, watch thier info in a comfortable page and even visit thier github account",
-      size: "Very Small",
-      builtWith:"React",
-      img: "githubFinder.PNG",
-      githubLink:"https://github.com/michaelDonchenko/Github-finder",
-      demoLink:"https://github-finder7438473.netlify.app/"
+    {
+      name: 'IT-Logger',
+      description:
+        'IT-Logger is an app for IT department, here the technichians can add/remove/update thier IT logs. Another feature is adding and delling technichians, And making the important in case it needs extra attention.',
+      size: 'Small',
+      builtWith: 'MERN Stack (Redux)',
+      img: 'itLogger.PNG',
+      githubLink: 'https://github.com/michaelDonchenko/IT-looger',
+      demoLink: 'https://it-looger.herokuapp.com/',
     },
-
-    { name: "Contact-Keeper",
-      description: "Contact Keeper is a full stack MERN app, you can make an account login and save your contacts by name/phone/email. More features are saving your contacts by type (personal/professional) and filtered search.",
-      size: "Small-Medium",
-      builtWith:"MERN Stack (Context-API)",
-      img: "contactKeeper.PNG",
-      githubLink:"https://github.com/michaelDonchenko/Contact-Keeper",
-      demoLink:"https://contact-keeper1212.herokuapp.com/"
-    }, 
   ]
 
   return (
     <div>
-      {
-        projects.map(project => <ProjectItem project={project} />)
-      }
+      {projects.map((project) => (
+        <ProjectItem project={project} />
+      ))}
     </div>
   )
 }
